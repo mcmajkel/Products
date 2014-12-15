5370285a475e81e6f30c78927ba15ef11f76f668
Michal Zlamaniec <mcmajk3l@gmail.com>
2014-12-15 21:57:11 +0100

    adding to datastore

--- src/com/example/shoppinglist/activities/ProductsListActivity.java
+++ src/com/example/shoppinglist/activities/ProductsListActivity.java
@@ -1,16 +1,22 @@
 package com.example.shoppinglist.activities;
 
+import java.io.IOException;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 
+import com.example.shoppinglist.CloudEndpointUtils;
 import com.example.shoppinglist.R;
 import com.example.shoppinglist.interfaces.ProductsDataSource;
 import com.example.shoppinglist.models.Product;
+import com.example.shoppinglist.productendpoint.Productendpoint;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.jackson2.JacksonFactory;
 
 public class ProductsListActivity extends ListActivity {
 	private ProductsDataSource datasource;
@@ -33,13 +39,15 @@ public class ProductsListActivity extends ListActivity {
 	    Product product = null;
 	    switch (view.getId()) {
 	    case R.id.add:
-	      
+//	      
 	      EditText productNameInput = (EditText) findViewById(R.id.productName);
 	      String productName = productNameInput.getText().toString();
-	      product = datasource.createProduct(productName);
-	      adapter.insert(product, 0);
+//	      product = datasource.createProduct(productName);
+//	      adapter.insert(product, 0);
+	      new AddToDataStoreTask().execute(productName);
 	      productNameInput.setText("");
 	      break;
+	      
 	    case R.id.delete:
 	      if (getListAdapter().getCount() > 0) {
 	        product = (Product) getListAdapter().getItem(0);
@@ -50,4 +58,42 @@ public class ProductsListActivity extends ListActivity {
 	    }
 	    adapter.notifyDataSetChanged();
 	  }
+	
+	private class AddToDataStoreTask extends AsyncTask<String, Void, Void> {
+
+	    /**
+	     * Calls appropriate CloudEndpoint to indicate that user checked into a place.
+	     *
+	     * @param params the place where the user is checking in.
+	     */
+
+		@Override
+		protected Void doInBackground(String... params) {
+			  com.example.shoppinglist.productendpoint.model.Product product = new com.example.shoppinglist.productendpoint.model.Product ();
+		      
+		      // Set the ID of the store where the user is. 
+		      // This would be replaced by the actual ID in the final version of the code. 
+		      product.setName(params[0].toString());
+
+		      Productendpoint.Builder builder = new Productendpoint.Builder(
+		          AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
+		          null);
+		          
+		      builder = CloudEndpointUtils.updateBuilder(builder);
+		      builder.setRootUrl("http://192.168.0.10:8888/_ah/api");
+		      Productendpoint endpoint = builder.build();
+		      
+
+		      try {
+		        endpoint.insertProduct(product).execute();
+		      } catch (IOException e) {
+		        // TODO Auto-generated catch block
+		        e.printStackTrace();
+		      }
+
+		      return null;
+		}
+	  }
+	
+	
 }
